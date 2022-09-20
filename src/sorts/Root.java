cc5b2945d75c8d9434e349bf3770043cd3f433ad
Vlad Solovey <81081329+soloVlad@users.noreply.github.com>
2022-09-20 21:34:54 +0300

    feat: add odd-even sorting algorithm (#3)

--- src/sorts/Root.java
+++ src/sorts/Root.java
@@ -165,4 +165,39 @@ public class Root {
 
         return resultArray;
     }
+
+    public int[] oddEvenSort(int[] arr, int n)
+    {
+        boolean isSorted = false;
+
+        while (!isSorted)
+        {
+            isSorted = true;
+            int temp =0;
+
+            for (int i=1; i<=n-2; i=i+2)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+
+            for (int i=0; i<=n-2; i=i+2)
+            {
+                if (arr[i] > arr[i+1])
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+
+        return arr;
+    }
 }
